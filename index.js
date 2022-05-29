065177589d5c84edcee1520898607e267b506cbc
Ossai Lucky <luckyblaqy@gmail.com>
2022-05-29 15:23:33 +0100

    added index.js file

---
+++ index.js
@@ -0,0 +1,28 @@
+const socialMedias = ["facebook", "instagram", "twitter", "tik tok"];
+
+for( let i = 0; i < socialMedias.length; i++){
+    console.log(socialMedias[i]);
+}
+
+const cryptocurrencies = ["Bitcoin", "Ethereum", "Terra", "dodge"];
+
+for(let i = 0; i < cryptocurrencies.length; i++){
+    console.log(cryptocurrencies[i]);
+}
+
+const courses = ["computer science", "Geology","medicine","Law","engineering"];
+
+for(let i = 0; i < courses.length; i++) {
+    console.log(courses[i])
+}
+
+const states = ["Rivers","Delta", "Imo ", "Lagos", "Edo"];
+for(let i = 0; i < states.length; i++){
+    console.log(states[i]);
+}
+
+const africaCountries = ["Nigeria", "South Africa", "Ghana", "senegal", "Algeria"]
+
+for(let i = 0; i < africaCountries.length; i++){
+    console.log(africaCountries[i]);
+}
